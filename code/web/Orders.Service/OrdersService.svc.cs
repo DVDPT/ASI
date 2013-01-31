4a40b1d2d1371e413436f102eb3d2a13948b4d74
João Silvestre <joaopaulosilvestre@hotmail.com>
2013-01-31 00:19:07 +0000

    Adicionado launcher para arrancar os serviços "Self Hosted"
    Configurações dos serviços passam a ser no App.config do launcher

---
+++ code/web/Orders.Service/OrdersService.svc.cs
@@ -0,0 +1,27 @@
+﻿using OrdersService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using Ninject;
+using DAL.Access.OrdersCenter;
+
+namespace OrdersService
+{
+    public class OrdersService : IOrdersService
+    {
+        public OrdersService()
+        {
+
+        }
+
+        public void PlaceOrder(OrderModel order)
+        {
+
+        }
+    }
+}
---
+++ code/web/ServicesLauncher/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Management.Service;
+using OrdersService;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServicesLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private volatile bool _ordersServiceStatus = false;
+        private volatile bool _managementServiceStatus = false;
+        private volatile bool _notificationsServiceStatus = false;
+
+        private ServiceHost _ordersHost = null;
+        private ServiceHost _managementHost = null;
+        private ServiceHost _notificationsHost = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BrowseOrdersLink.RequestNavigate += HandleRequest;
+            BrowseManagementLink.RequestNavigate += HandleRequest;
+            BrowseNotificationsLink.RequestNavigate += HandleRequest;   
+        }
+
+        private void HandleRequest(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void StartOrdersService_Click(object sender, RoutedEventArgs e)
+        {
+            _ordersServiceStatus = !_ordersServiceStatus;   
+ 
+            if (_ordersServiceStatus)
+            {
+                StartOrdersService.Content = "Stop";
+                OrdersServiceStatus.Fill = new SolidColorBrush { Color = Color.FromRgb(0, 255, 0) };
+                BrowseOrders.Visibility = Visibility.Visible;
+
+                _ordersHost = new ServiceHost(typeof(OrdersService.OrdersService));
+                _ordersHost.Open();
+
+                BrowseOrdersLink.NavigateUri = _ordersHost.BaseAddresses.First();
+                BrowseOrders.Text += _ordersHost.BaseAddresses.First().AbsoluteUri;
+            }
+            else
+            {
+                StartOrdersService.Content = "Start";
+                OrdersServiceStatus.Fill = new SolidColorBrush { Color = Color.FromRgb(255, 0, 0) };
+                BrowseOrders.Visibility = Visibility.Collapsed;
+
+                _ordersHost.Close();
+            }         
+        }
+
+        private void StartManagementService_Click(object sender, RoutedEventArgs e)
+        {
+            _managementServiceStatus = !_managementServiceStatus;
+
+            if (_managementServiceStatus)
+            {
+                StartManagementService.Content = "Stop";
+                ManagementServiceStatus.Fill = new SolidColorBrush { Color = Color.FromRgb(0, 255, 0) };
+                BrowseManagement.Visibility = Visibility.Visible;
+
+                _managementHost = new ServiceHost(typeof(CustomerOrderReceiverService));
+                _managementHost.Open();
+
+                BrowseManagementLink.NavigateUri = _managementHost.BaseAddresses.First();
+                BrowseManagement.Text += _managementHost.BaseAddresses.First().AbsoluteUri;
+            }
+            else
+            {
+                StartManagementService.Content = "Start";
+                ManagementServiceStatus.Fill = new SolidColorBrush { Color = Color.FromRgb(255, 0, 0) };
+                BrowseManagement.Visibility = Visibility.Collapsed;
+
+                _managementHost.Close();
+            } 
+        }
+
+        private void StartNotificationsService_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (_ordersHost != null)
+            {
+                using (_ordersHost)
+                {
+                    if (_ordersServiceStatus)
+                    {
+                        _ordersHost.Close();
+                    }
+                }
+            }
+
+            if (_managementHost != null)
+            {
+                using (_managementHost)
+                {
+                    if (_managementServiceStatus)
+                    {
+                        _managementHost.Close();
+                    }
+                }
+            }
+
+            if (_notificationsHost != null)
+            {
+                using (_notificationsHost)
+                {
+                    if (_notificationsServiceStatus)
+                    {
+                        _notificationsHost.Close();
+                    }
+                }
+            }
+            
+            base.OnClosed(e);
+        }
+    }
+}
